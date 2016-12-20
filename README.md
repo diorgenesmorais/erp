7e6f6c976341128cb19bc2bb7d9a93ab5d02d2f2
diorgenesmorais <diorgenesmorais@gmail.com>
2016-12-20 19:43:01 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Projeto web com Spring web MVC
---
+++ src/main/java/com/dms/erp/config/init/AppInitializer.java
@@ -0,0 +1,25 @@
+package com.dms.erp.config.init;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class AppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected String[] getServletMappings() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
