9d3ffbceacdd1fef197bfe6811cd7254f9a9df8e
diorgenesmorais <diorgenesmorais@gmail.com>
2016-12-28 08:32:19 -0300

    implementando ordenação na view por SKU e nome  Mudanças a serem submetidas: 	modificado: src/main/java/com/dms/erp/controller/page/PageWrapper.java 	modificado: src/main/resources/templates/cerveja/pesquisaCervejas.html

--- src/main/java/com/dms/erp/controller/page/PageWrapper.java
+++ src/main/java/com/dms/erp/controller/page/PageWrapper.java
@@ -5,6 +5,7 @@ import java.util.List;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Sort.Order;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import org.springframework.web.util.UriComponentsBuilder;
 
@@ -54,4 +55,44 @@ public class PageWrapper<T> {
 		 */
 		return uriBuilder.replaceQueryParam("page", current).build(true).encode().toUriString();
 	}
+
+	public String urlOrderBy(String property) {
+		UriComponentsBuilder uriComponentBuilder = UriComponentsBuilder
+				.fromUriString(uriBuilder.build(true).encode().toUriString());
+
+		String sortValue = String.format("%s,%s", property, reverseOrder(property));
+
+		return uriComponentBuilder.replaceQueryParam("sort", sortValue).build(true).encode().toUriString();
+	}
+
+	/*
+	 * Returns null if not ordered.
+	 */
+	private Order getOrder(String property) {
+		return page.getSort() != null ? page.getSort().getOrderFor(property) : null;
+	}
+
+	public boolean isOrder(String property) {
+		return getOrder(property) != null;
+	}
+
+	/*
+	 * Returns true if ascending, returns false if not ordered, that is, being
+	 * false does not guarantee that it is not downward.
+	 */
+	public boolean isAscending(String property) {
+		Order order = getOrder(property);
+		return order != null && order.isAscending();
+	}
+
+	public String reverseOrder(String property) {
+		String direction = "asc";
+
+		if (isAscending(property)) {
+			direction = "desc";
+		}
+
+		return direction;
+	}
+
 }
