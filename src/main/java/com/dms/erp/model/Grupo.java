a22e856013c8424ace3fe5135ff9f2007c298311
diorgenesmorais <diorgenesmorais@gmail.com>
2017-01-12 11:17:29 -0300

    Criei os modelos baseado na última migração

---
+++ src/main/java/com/dms/erp/model/Grupo.java
@@ -0,0 +1,86 @@
+package com.dms.erp.model;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+
+import org.hibernate.validator.constraints.NotBlank;
+
+@Entity
+@Table(name = "grupo")
+public class Grupo implements Serializable {
+
+	private static final long serialVersionUID = -8032498740562495706L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@NotBlank(message = "Nome do grupo é obrigatório")
+	@Column(nullable = false)
+	private String name;
+
+	@ManyToMany
+	@JoinTable(name = "grupo_permissao", joinColumns = {
+			@JoinColumn(name = "grupo_id", nullable = false) }, inverseJoinColumns = {
+					@JoinColumn(name = "permissao_id", nullable = false) })
+	private List<Permissao> permissoes;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public List<Permissao> getPermissoes() {
+		return permissoes;
+	}
+
+	public void setPermissoes(List<Permissao> permissoes) {
+		this.permissoes = permissoes;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Grupo other = (Grupo) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+}
---
+++ src/main/java/com/dms/erp/model/Usuario.java
@@ -0,0 +1,136 @@
+package com.dms.erp.model;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.validator.constraints.Email;
+import org.hibernate.validator.constraints.NotBlank;
+
+@Entity
+@Table(name = "usuario")
+public class Usuario implements Serializable {
+
+	private static final long serialVersionUID = 180384153860549271L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@NotBlank(message = "Nome do usuário é obrigatório")
+	@Column(length = 50, nullable = false)
+	private String usename;
+
+	@Email
+	@Column(length = 64, nullable = false)
+	private String email;
+
+	@Column(length = 64, nullable = false)
+	private String password;
+
+	@Column(nullable = false)
+	private Boolean active;
+
+	@NotNull(message = "Data de nascimento é obrigatória")
+	@Column(name = "birth_date")
+	private LocalDate birthDate;
+
+	@NotNull(message = "Selecione pelo menos um grupo")
+	@ManyToMany
+	@JoinTable(name = "usuario_grupo", joinColumns = {
+			@JoinColumn(name = "usuario_id", nullable = false) }, inverseJoinColumns = {
+					@JoinColumn(name = "grupo_id", nullable = false) })
+	private List<Grupo> grupos;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getUsename() {
+		return usename;
+	}
+
+	public void setUsename(String usename) {
+		this.usename = usename;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public Boolean getActive() {
+		return active;
+	}
+
+	public void setActive(Boolean active) {
+		this.active = active;
+	}
+
+	public LocalDate getBirthDate() {
+		return birthDate;
+	}
+
+	public void setBirthDate(LocalDate birthDate) {
+		this.birthDate = birthDate;
+	}
+
+	public List<Grupo> getGrupos() {
+		return grupos;
+	}
+
+	public void setGrupos(List<Grupo> grupos) {
+		this.grupos = grupos;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Usuario other = (Usuario) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+}
