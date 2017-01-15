87e80f1fb3d5f94ff92a8c90793173751edcf46e
diorgenesmorais <diorgenesmorais@gmail.com>
2017-01-14 21:41:18 -0300

    configurando Spring Security no projeto

---
+++ src/main/java/com/dms/erp/config/init/SecurityInitializer.java
@@ -0,0 +1,7 @@
+package com.dms.erp.config.init;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityInitializer extends AbstractSecurityWebApplicationInitializer {
+
+}
