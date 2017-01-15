00901501b112e0f28af914a47cc7af958ca92cac
diorgenesmorais <diorgenesmorais@gmail.com>
2017-01-15 14:27:19 -0300

    Tela de login customizada

--- src/main/java/com/dms/erp/config/SecurityConfig.java
+++ src/main/java/com/dms/erp/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.dms.erp.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -17,13 +18,21 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	}
 	
 	@Override
+	public void configure(WebSecurity web) throws Exception {
+		web.ignoring()
+		.antMatchers("/layout/**")
+		.antMatchers("/images/**");
+	}
+	
+	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http
 			.authorizeRequests()
-				.anyRequest()
-					.authenticated()
+				.anyRequest().authenticated()
 					.and()
 					.formLogin()
+					.loginPage("/login")
+					.permitAll()
 					.and()
 					.csrf().disable();
 	}
---
+++ src/main/java/com/dms/erp/controller/LoginController.java
@@ -0,0 +1,19 @@
+package com.dms.erp.controller;
+
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class LoginController {
+
+	@GetMapping("/login")
+	public ModelAndView login(@AuthenticationPrincipal User user) {
+		if (user != null) {
+			return new ModelAndView("redirect:/cervejas");
+		}
+		return new ModelAndView("login");
+	}
+}
