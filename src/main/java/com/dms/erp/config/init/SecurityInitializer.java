1c6f97acaba02768b5d8d49e50e229b2f431b281
diorgenesmorais <diorgenesmorais@gmail.com>
2017-01-17 17:08:57 -0300

    Aumentando a segurança

--- src/main/java/com/dms/erp/config/init/SecurityInitializer.java
+++ src/main/java/com/dms/erp/config/init/SecurityInitializer.java
@@ -1,21 +1,34 @@
 package com.dms.erp.config.init;
 
+import java.util.EnumSet;
+
 import javax.servlet.FilterRegistration;
 import javax.servlet.ServletContext;
+import javax.servlet.SessionTrackingMode;
 
 import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
 import org.springframework.web.filter.CharacterEncodingFilter;
 
 public class SecurityInitializer extends AbstractSecurityWebApplicationInitializer {
 
-	// corrigindo o problema de encode devido a implementação do Spring Security
 	@Override
 	protected void beforeSpringSecurityFilterChain(ServletContext servletContext) {
-		// definir em quantos segundos a sessão irá expirar independente de o usuário está mexendo ou não
-		//servletContext.getSessionCookieConfig().setMaxAge(1200); // 1200 = 20 minutos
-		
-		FilterRegistration.Dynamic characterEncoderFilter = servletContext.addFilter("encoderFilter"
-				, new CharacterEncodingFilter());
+		/**
+		 * definir em quantos segundos a sessão irá expirar independente de o
+		 * usuário está mexendo ou não (1200 = 20 minutos)
+		 */
+		// servletContext.getSessionCookieConfig().setMaxAge(1200);
+
+		/**
+		 * impedir que o número da sessão apareça na URL
+		 */
+		servletContext.setSessionTrackingModes(EnumSet.of(SessionTrackingMode.COOKIE));
+
+		/**
+		 * corrigindo o problema de encode devido a implementação do Spring Security
+		 */
+		FilterRegistration.Dynamic characterEncoderFilter = servletContext.addFilter("encoderFilter",
+				new CharacterEncodingFilter());
 		characterEncoderFilter.setInitParameter("encoding", "UTF-8");
 		characterEncoderFilter.setInitParameter("forceEncoding", "true");
 		characterEncoderFilter.addMappingForUrlPatterns(null, false, "/*");
