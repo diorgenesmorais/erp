cdb2b58b58c34e9d4c81b11aad8e9bc9e374ebc9
diorgenesmorais <diorgenesmorais@gmail.com>
2017-01-18 10:21:37 -0300

    Implementando a página 500

--- src/main/java/com/dms/erp/controller/ErrosController.java
+++ src/main/java/com/dms/erp/controller/ErrosController.java
@@ -2,12 +2,18 @@ package com.dms.erp.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class ErrosController {
 
 	@GetMapping("/404")
-	public String error() {
+	public String pageNotFound() {
 		return "404";
+	}
+
+	@PostMapping("/500")
+	public String internalError() {
+		return "500";
 	}
 }
