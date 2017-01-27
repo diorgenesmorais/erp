09ee93e09cb9f12198180f0dd0928c290e3922c5
diorgenesmorais <diorgenesmorais@gmail.com>
2017-01-27 15:07:33 -0300

    Modifiquei o pacote também fiz as anotações: @Component para que a classe seja um componente do Spring para que ela possa ser injetada. Nota: para que o Spring encontre está classe em um pacote específico deve ser configurado na classe de configuração em @ComponentScan(basePackageClasses = { nome do pacote }) @see WebConfig Também fiz anotação com @SessionScope (para injetar uma classe por sessão), que após a versão 4.3 substituiu o @Scope(value = WebApplicationContext.SCOPE_SESSION, proxyMode = ScopedProxyMode.TARGET_CLASS)

--- src/main/java/com/dms/erp/session/TabelaItensVenda.java
+++ src/main/java/com/dms/erp/session/TabelaItensVenda.java
@@ -1,12 +1,17 @@
-package com.dms.erp.venda;
+package com.dms.erp.session;
 
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.annotation.SessionScope;
+
 import com.dms.erp.model.Cerveja;
 import com.dms.erp.model.ItemVenda;
 
+@SessionScope
+@Component
 public class TabelaItensVenda {
 
 	private List<ItemVenda> itens = new ArrayList<>();
